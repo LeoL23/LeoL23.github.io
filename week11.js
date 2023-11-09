334a58d32c037b83835fcb7086413f9554c817a0
LeoL23 <lela4111@colorado.edu>
2023-11-08 17:20:35 -0700

    week11

--- week11.js
+++ week11.js
@@ -1,6 +1,5 @@
 console.log("Making sure this test works");
 
-
 const x = 1;
 const y = 2;
 
@@ -22,13 +21,36 @@ document.addEventListener
     canvas.height = document.documentElement.clientHeight;
 });
 
+//console.log(random(2));
+
 function random(number) {
-    
+    return Math.floor(Math.random () * number);
+}
+
+function random2(number) {
+    return(Math.random() * number);
 }
 
 function draw() {
-
-
+    ctx.clearRect(0,0,canvas.width, canvas.height);
+    for (let i = 0; i<666; i++) {
+        ctx.beginPath();
+        let red = random(255);
+        let green = random(255);
+        let blue = random(255);
+        let alpha = random2(1);
+        let color = "rgba("+red+","+green+","+blue+","+alpha+")";
+        console.log(color);
+        ctx.fillStyle = color;
+        ctx.arc(
+            random(canvas.width),
+            random(canvas.height),
+            random(50),
+            0,
+            2 * Math.PI,
+        );
+        ctx.fill();
+    }
 }
 
 btn.addEventListener("click", draw);
