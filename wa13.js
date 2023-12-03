f6fc5fb9f50633ec481650d30bfe87910670d2df
LeoL23 <lela4111@colorado.edu>
2023-12-03 15:45:19 -0700

    wa13

---
+++ wa13.js
@@ -0,0 +1,26 @@
+console.log("Making sure this  works");
+
+class Company {
+    "companyName": "Tech Stars", //Q2
+    "website": "Website", //Q2
+    "employees": [ 
+        {
+            "firstName": "Sam",
+            "department": "Tech",
+            "Salary": "Manager",
+            "raiseEligible": "40000",
+        },
+        {
+            "firstName": "Mary",
+            "department": "Finance",
+            "Salary": "Trainee",
+            "raiseEligible": "18500",    
+        },
+        {
+            "firstName": "Bill",
+            "department": "HR",
+            "Salary": "Executive",
+            "raiseEligible": "21200",    
+        }
+    ]
+}
