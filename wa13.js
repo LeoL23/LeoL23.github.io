3179c62aa5c34f39bef713284e022b0f914d7984
LeoL23 <lela4111@colorado.edu>
2023-12-03 22:25:25 -0700

    wa13

--- wa13.js
+++ wa13.js
@@ -1,5 +1,48 @@
 console.log("Making sure this  works");
 
+let HR = {
+
+    "employees": [{
+    "name": "Sam",
+    "department": "Tech",
+    "designation": "Manager",
+    "salary": 40000,
+    "raise-eligible": true,
+    },
+    {
+    "name": "Mary",
+    "department": "Finance",
+    "designation": "Trainee",
+    "salary": 18500,
+    "raise-eligible": true,
+    },
+    {
+    "name": "Bill",
+    "department": "HR",
+    "designation": "Executive",
+    "salary": 212000,
+    "raise-eligible": false,
+    }
+]
+}
+
+let company = {
+    "companyInfo": [
+        {
+            "companyName": "Tech Stars",
+            "Website": "www.techstars.site",
+            "employees": HR["employees"],
+        }
+    ]
+}
+
+
+
+
+
+
+
+
 class Company {
     "companyName": "Tech Stars", //Q2
     "website": "Website", //Q2
